4feb03cf624c104b5f13937d4fbf20f11962e528
Jim <jim@advancebass.com>
2026-03-05 15:54:56 -0800

    log env and add cat emoji

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  console.log("env", import.meta.env);
+
   return (
     <>
       <div>
@@ -16,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React 🐱</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
